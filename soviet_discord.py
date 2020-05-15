800180fb28fecf3d1a171316dcd1c0b3a772517d
WellsGoFar <manuashwinbhatnagar@gmail.com>
2020-05-14 22:41:58 -0700

    new commit

--- soviet_discord.py
+++ soviet_discord.py
@@ -11,7 +11,12 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to discord')
-    for guild in client.guilds:
-        print(f'{guild.name}(id: {guild.id})')
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    
+    print(f'{guild.name}')
+    members = "\n - ".join([members.name for members in guild.members])
+    total = guild.member_count
+    # print(f'Server members: \n {members}')
+    print(f'total members are: {total}')
 
 client.run(TOKEN)
