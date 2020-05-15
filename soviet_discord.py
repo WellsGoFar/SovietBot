209564c495167f93d16136c35a75834716a6f1b9
WellsGoFar <manuashwinbhatnagar@gmail.com>
2020-05-14 21:50:16 -0700

    first guild data

--- soviet_discord.py
+++ soviet_discord.py
@@ -5,13 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('GUILD')
 client = discord.Client()
-
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to discord')
-    print(f'Logged in as: {client.user.name}')
-    print(f'With ID: {client.user.id}')
+    for guild in client.guilds:
+        print(f'{guild.name}(id: {guild.id}')
 
 client.run(TOKEN)
