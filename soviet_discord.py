35431a00d5f644486e35670ef5ebc26080983a69
WellsGoFar <manuashwinbhatnagar@gmail.com>
2020-05-14 21:34:28 -0700

    bot is live

---
+++ soviet_discord.py
@@ -0,0 +1,17 @@
+import os
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(f'Logged in as: {client.user.name}')
+    print(f'With ID: {client.user.id}')
+
+
+client.run(TOKEN)
