2742687ce1fa0eb06a0f55c0d8ced6fd06960bcd
WellsGoFar <manuashwinbhatnagar@gmail.com>
2020-05-16 19:30:23 -0700

    first live version of bot

--- soviet_discord.py
+++ soviet_discord.py
@@ -7,6 +7,7 @@ from discord.ext import commands, tasks
 from time import gmtime, strftime
 from soviet_reddit import *
 import asyncio
+import urllib.request
 
 
 bot = commands.Bot(command_prefix='pp ')
@@ -14,8 +15,10 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('GUILD')
 GUILDID = int(os.getenv('GUILDID'))
-BOTTESTCHANNEL = os.getenv('BOTTESTCHANNEL')
+FACTSCHANNEL = os.getenv('FACTSCHANNEL')
+MEMECHANNEL = os.getenv('MEMECHANNEL')
 LOGCHANNEL = os.getenv('LOGCHANNEL')
+NSFWCHANNEL = os.getenv('NSFWCHANNEL')
 bot = commands.Bot(command_prefix=['pp ','Pp ','pP ', 'PP'])
 # guild = bot.get_guild(id=GUILDID)
 
@@ -36,7 +39,7 @@ class Greetings(commands.Cog):
         channel = [channel for channel in all_channels if channel.name == LOGCHANNEL][0]
         # member_mention = '<@{}>'.format(member.id)
         if channel is not None:
-            await channel.send('Hey {0.mention} now serves the Soviet Union. Welcome to the **{1}**, comrad! {2}{3}.'.format(member, member.guild.name, str(emoji1), str(emoji2)))
+            await channel.send('Hey! {0.mention} now serves the Soviet Union. Welcome to the **{1}**, comrade! {2}{3}.'.format(member, member.guild.name, str(emoji1), str(emoji2)))
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
@@ -44,16 +47,15 @@ class Greetings(commands.Cog):
         emoji1 = discord.utils.get(member.guild.emojis, name='roadwarrior')
         channel = [channel for channel in all_channels if channel.name == LOGCHANNEL][0]
         if channel is not None:
-            await channel.send('**{0.name}** is no longer a comrad. {1}'.format(member, str(emoji1)))
+            await channel.send('**{0.name}** is no longer a comrade. {1}'.format(member, str(emoji1)))
 
-    @commands.command(help = 'the bot will say hello to you because your friends won\'t')
+    @commands.command(help = ':: the bot will say hello to you because your friends won\'t')
     async def hello(self, ctx, *, member: discord.Member = None):
         member = member or ctx.author
-        member_mention = '<@{}>'.format(member.id)
         if self._last_member is None or self._last_member.id != member.id:
             await ctx.send('Hello {0.mention}~'.format(member))
         else:
-            emoji_saved = discord.utils.get(member.guild.emojis, name="theGayEmoji")
+            emoji_saved = discord.utils.get(member.guild.emojis, name="triggered")
             print(emoji_saved)
             await ctx.send('{0.mention}... Once is enough, get a life.{1}'.format(member,str(emoji_saved)))
         self._last_member = member
@@ -61,41 +63,84 @@ class Greetings(commands.Cog):
 
 client = discord.Client()
 
-class funFact(commands.Cog):
+class reddit_handler(commands.Cog):
     def __init__(self,bot):
         self.index = 0
         self.bot = bot
         self.throw_fact.start()
+        self.throw_meme.start()
+        self.throw_pifs.start()
 
     def cog_unload(self):
         self.throw_fact.cancel()
+        self.throw_meme.cancel()
+        self.throw_pifs.cancel()
 
     @tasks.loop(hours=8.0)
     async def throw_fact(self):
-        # print(self.index)
-        self.index += 1
         
         guild = bot.get_guild(id=GUILDID)
         for channel in guild.channels:
-            if channel.name == BOTTESTCHANNEL:
+            if channel.name == FACTSCHANNEL:
                 break
         #printing guild name and text channel object type for debugging
-        print(guild)
+        # print(guild)
         # print(type(guild))
-        print(type(channel))
+        # print(type(channel))
         #sends a message for looping test
         titles, links = get_til()
         for i in range(len(titles)):
             message_string = titles[i]
             message_url = links[i]
+            print('SENDING FACT....')
             await channel.send(str("""```css\n{}```\n<{}>""".format(message_string, message_url)))
-            # await asyncio.sleep(5400)
-            await asyncio.sleep(10)
+            await asyncio.sleep(5400)
+            # await asyncio.sleep(10)
 
+    @tasks.loop(hours=6.0)
+    async def throw_meme(self):
+        guild = bot.get_guild(id=GUILDID)
+        for channel in guild.channels:
+            if channel.name == MEMECHANNEL:
+                break
+        titles, links = get_meme()
+        for i in range(len(titles)):
+            meme_title = titles[i]
+            meme_url = links[i]
+            embed = discord.Embed(
+                title = meme_title,
+                colour = discord.Colour.blue()
+                )
+            embed.set_image(url=meme_url)
+            print('SENDING MEME....')
+            await channel.send(embed=embed)
+            await asyncio.sleep(100)
+
+    @tasks.loop(hours=6.0)
+    async def throw_pifs(self):
+        guild = bot.get_guild(id=GUILDID)
+        for channel in guild.channels:
+            if channel.name == NSFWCHANNEL:
+                break
+        links = get_pifs()
+        for link in links:
+            print('SENDING PIFS....')
+            await channel.send(link)
+            await asyncio.sleep(1440)
 
     @throw_fact.before_loop
     async def before_throw_fact(self):
         print('waiting...')
+        await self.bot.wait_until_ready()
+    
+    @throw_meme.before_loop
+    async def before_throw_meme(self):
+        print('meme waiting...')
+        await self.bot.wait_until_ready()
+
+    @throw_pifs.before_loop
+    async def before_throw_pif(self):
+        print('pif gif waiting...')
         await self.bot.wait_until_ready()
 
 class ImDad(commands.Cog):
@@ -110,9 +155,8 @@ class ImDad(commands.Cog):
         cykaObj = re.compile(r'cyka', re.IGNORECASE)
         dadObj = re.compile(r'^im |^i\'m | im | i\'m ', re.IGNORECASE)
         if cykaObj.search(message.content):
-            print(message.channel)
+            # print(message.channel)
             await message.channel.send("cyka blyat")
-
 
         if dadObj.search(message.content):
             dadName = message.content[(dadObj.search(message.content).span()[1]): ]
@@ -124,15 +168,36 @@ class ImDad(commands.Cog):
             else: 
                 await message.channel.send(f'Hi {dadName}, I\'m dad')
 
-@bot.command(name='tripo',pass_context=True,help = 'tripoloski babyyyy')
-async def tripo(ctx):
-    response = "tripo tripo tripoloski"
-    print(response)
-    await ctx.send(response)
+
+class bot_commands(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
+
+    @commands.command(help = ':: tripoloski babyyyy')
+    async def tripo(self, ctx):
+        response = "Tri poloski, tripo tri poloski - Три полоски, три по три полоски"
+        await ctx.send(response)
+
+    @commands.command(help = ':: clear messages, accepts amount and defaults to 10')
+    @commands.has_permissions(manage_messages = True)
+    async def clear(self,ctx,amount=10):
+        await ctx.channel.purge(limit=amount+1)
+        await ctx.channel.send("Cleared **{}** messages".format(amount+1))
+        await asyncio.sleep(1.5)
+        await ctx.channel.purge(limit=1)
+        
 
 
-bot.add_cog(funFact(bot))
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            embed = discord.Embed(title="You don't have the permission",
+                colour = discord.Colour.blue())
+            await ctx.channel.send(embed=embed)
+
+bot.add_cog(reddit_handler(bot))
 bot.add_cog(Greetings(bot))
 bot.add_cog(ImDad(bot))
+bot.add_cog(bot_commands(bot))
 
 bot.run(TOKEN)
--- soviet_reddit.py
+++ soviet_reddit.py
@@ -1,23 +1,26 @@
 import praw
 import pickle
+import urllib.request
 reddit = praw.Reddit('bot1')
 
-# for submission in reddit.subreddit('todayilearned').hot(limit=3):
+# for submission in reddit.subreddit('dankmemes').hot(limit=15):
 
-#     print(submission.id)
-#     print(submission.title)
-#     print(submission.url)
-#     print(submission.permalink)
-#     redditor = submission.author
-#     print(redditor)
-#     print(str(redditor.link_karma))
-#     print("\n\n")
+#     if (not submission.stickied):
+#         print(submission.id)
+#         print(submission.title)
+#         print(submission.url)
+#         print(submission.permalink)
+#         redditor = submission.author
+#         print(redditor)
+#         print(str(redditor.link_karma))
+#         print("\n\n")
 
 def get_til():
     titles = []
     links = []
     posts = []
     new_posts = []
+    pinned_post_counter = 0
     with open('tracks/til_posts.txt', 'r') as filehandler:
         for line in filehandler:
             currentPost = line[:-1]
@@ -29,20 +32,83 @@ def get_til():
 
     for submission in reddit.subreddit('todayilearned').hot(limit=5):
         
+        if not submission.stickied:
+            if submission.id in posts:
+                continue
+
+            else:
+                titles.append(submission.title)
+                links.append(submission.url)
+                new_posts.append(submission.id)
+
+    posts = posts + new_posts            
+        
+    with open('tracks/til_posts.txt', 'w') as filehandler:
+        for listitem in posts:
+            filehandler.write('%s\n' % listitem)
+
+    return titles, links
+
+def get_meme():
+    titles, links, posts, new_posts = [], [], [], []
+    with open('tracks/meme_posts.txt', 'r') as filehandler:
+        for line in filehandler:
+            currentPost = line[:-1]
+            posts.append(currentPost)
+    
+    if len(posts) > 100:
+        posts = posts[49:]
+
+    meme_multireddit = [mult for mult in reddit.redditor("69sloth").multireddits() if mult.path == '/user/69sloth/m/memes_for_bot']
+    
+    for submission in meme_multireddit[0].hot(limit=12):
         if submission.id in posts:
             continue
+        else:
+            urllib.request.urlretrieve(submission.url, "dank_memes/{}.jpg".format(submission.id))
+            titles.append(submission.title)
+            links.append(submission.url)
+            new_posts.append(submission.id)
+            # print(submission.permalink)
 
+    posts = posts + new_posts
+    
+    with open('tracks/meme_posts.txt', 'w') as filehandler:
+        for listitem in posts:
+            filehandler.write('%s\n' % listitem)  
+    
+    return titles, links
+
+def get_pifs():
+    titles, links, posts, new_posts = [], [], [], []
+    with open('tracks/pifs.txt','r') as filehandler:
+        for line in filehandler:
+            currentPost = line[:-1]
+            posts.append(currentPost)
+        
+    if len(posts) > 200:
+        posts = posts[99:]
+
+    nsfw_multireddit = [mult for mult in reddit.redditor("69sloth").multireddits() if mult.path == '/user/69sloth/m/nsfw_for_bot']
+
+    for submission in nsfw_multireddit[0].hot(limit=15):
+
+        if submission.id in posts:
+            continue
         else:
             titles.append(submission.title)
             links.append(submission.url)
             new_posts.append(submission.id)
-
-    with open('tracks/til_posts.txt', 'a') as filehandler:
-        for listitem in new_posts:
+        
+    posts = posts + new_posts
+    
+    with open('tracks/pifs.txt', 'w') as filehandler:
+        for listitem in posts:
             filehandler.write('%s\n' % listitem)
-    
-    return titles, links
-    
-if __name__ == "__main__":
-    titles, links = get_til()
-    print(len(titles), len(links))
+
+    return links
+
+# if __name__ == "__main__":
+
+    # get_pifs()
+    # print(links)
