37e3243efb638cd83a08d12c2b2ba077fc57d661
WellsGoFar <manuashwinbhatnagar@gmail.com>
2020-05-21 14:51:05 -0700

    added f counter

--- src/cogs/imdad.py
+++ src/cogs/imdad.py
@@ -3,13 +3,20 @@ import re
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
 import asyncio
+import pymongo
 
 class ImDad(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
+        self.myclient = pymongo.MongoClient("mongodb://localhost:27017/") 
+        self.mydb = self.myclient["tesdb"] 
+        self.mycol = self.mydb["testcollection"]
 
     @commands.Cog.listener()
     async def on_message(self,message):
+
+        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="pp help"))
+
         if message.author == self.bot.user or message.author.bot:
             return 
 
@@ -29,10 +36,17 @@ class ImDad(commands.Cog):
             else: 
                 await message.channel.send(f'Hi {dadName}, I\'m dad')
 
-    def get_remind_embed(self):
-        return discord.Embed(title="Cyka blyat! That's not the correct syntax for that command",
-                description='This command accepts two arguments: time to remind and a reason \n\n The correct way of writing the command is: **pp remindme \{number\}\{s/m/h/d for seconds/minutes/hours/days respectively\} reason**\n\n example: pp remindme 2m do my homework :(',
-                colour = discord.Colour.blue())
+        if message.content.lower() == 'f':
+            mydoc = self.mycol.find()
+            myquery = { "guildID": str(message.guild.id) }
+            # mydoc = self.mycol.find() 
+            # for x in mydoc:
+            newvalues = { "$inc": { 'f_count': +1 } }
+            self.mycol.update_one(myquery, newvalues)
+            for x in mydoc:
+                if x['guildID'] == str(message.guild.id):
+                    await message.channel.send('f counter: {}'.format(x['f_count'] ))
+
 
 def setup(bot):
     bot.add_cog(ImDad(bot))
