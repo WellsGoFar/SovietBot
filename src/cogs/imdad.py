ea92f958d3a0f1aa9bcf12e7d323a69e2ea362ed
WellsGoFar <manuashwinbhatnagar@gmail.com>
2020-05-27 22:56:13 -0700

    changed the database update logic for better performance

--- src/cogs/imdad.py
+++ src/cogs/imdad.py
@@ -11,6 +11,29 @@ class ImDad(commands.Cog):
         self.myclient = pymongo.MongoClient("mongodb://localhost:27017/") 
         self.mydb = self.myclient["tesdb"] 
         self.mycol = self.mydb["testcollection"]
+        self.f_count = {}
+        self.f_counts = {}
+        self.update_server_f_counts.start()
+        
+    def cog_unload(self):
+        self.update_server_f_counts.cancel()
+
+    def get_server_f_counts(self):
+        for x in self.mycol.find():
+            self.f_counts[x['guildID']] = x['f_count']
+        return self.f_counts
+
+    @tasks.loop(minutes=30.0)
+    async def update_server_f_counts(self):
+        try:
+            for i in self.f_count:
+                myquery = {"guildID": i}
+                newvalues = { "$set": { "f_count": self.f_count[i] } } #change
+                self.mycol.update_one(myquery, newvalues)
+            self.f_counts.update(self.f_count)
+            self.f_count = {}
+        except Exception as e:
+            print(e)
 
     @commands.Cog.listener()
     async def on_message(self,message):
@@ -36,17 +59,31 @@ class ImDad(commands.Cog):
             else: 
                 await message.channel.send(f'Hi {dadName}, I\'m dad')
 
-        if message.content.lower() == 'f':
-            mydoc = self.mycol.find()
-            myquery = { "guildID": str(message.guild.id) }
-            # mydoc = self.mycol.find() 
-            # for x in mydoc:
-            newvalues = { "$inc": { 'f_count': +1 } }
-            self.mycol.update_one(myquery, newvalues)
-            for x in mydoc:
-                if x['guildID'] == str(message.guild.id):
-                    await message.channel.send('Respects were paid {} times.'.format(x['f_count'] ))
 
+        if message.content.lower() == 'f':
+            try:
+                if self.f_counts:
+                    guildID = str(message.guild.id)
+                    if guildID in self.f_count:
+                        self.f_count[guildID] += 1
+                    else:
+                        self.f_count[guildID] = self.f_counts[guildID] + 1
+                    await message.channel.send('Respects were paid {} times.'.format(self.f_count[guildID]))
+                else:
+                    self.f_counts = self.get_server_f_counts()
+                    guildID = str(message.guild.id)
+                    if guildID in self.f_count:
+                        self.f_count[guildID] += 1
+                    else:
+                        self.f_count[guildID] = self.f_counts[guildID] + 1
+                    await message.channel.send('Respects were paid {} times.'.format(self.f_count[guildID]))
+            except Exception as e:
+                await message.channel.send(e)
+
+    @update_server_f_counts.before_loop
+    async def before_server_f_count_update(self):
+        print('server update waiting...')
+        await self.bot.wait_until_ready()
 
 def setup(bot):
     bot.add_cog(ImDad(bot))
