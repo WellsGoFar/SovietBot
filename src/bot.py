3791db4643f837b61ea7976de54dcf7b4d37c5b5
WellsGoFar <manuashwinbhatnagar@gmail.com>
2021-03-11 03:15:18 -0800

    fixed the 'playing' command bug and added intents

--- src/bot.py
+++ src/bot.py
@@ -5,18 +5,23 @@ from dotenv import load_dotenv
 
 import json
 from discord.ext import commands, tasks
-
+intents = discord.Intents.all()
+# intents.members = True
+# intents.presences = True
 
 bot = commands.Bot(command_prefix='pp ')
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('GUILD')
-bot = commands.Bot(command_prefix=['pp ','Pp ','pP ', 'PP'])
+bot = commands.Bot(command_prefix=['pp ','Pp ','pP ', 'PP'], intents = intents)
 
 bot.load_extension("cogs.bot_commands_file")
 bot.load_extension("cogs.greetings")
 bot.load_extension("cogs.imdad")
 bot.load_extension("cogs.reddithandler")
+bot.load_extension('cogs.poll')
+bot.load_extension('cogs.npoll')
+bot.load_extension('cogs.among_us')
 bot.load_extension("cogs.bot_management")
 bot.load_extension("cogs.reminder")
 bot.load_extension("cogs.help")
